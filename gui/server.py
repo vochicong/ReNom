afc9de808b01d93444edebb1ce64458b0df37b9e
numajiri <numajiri@numa.local>
2017-09-29 09:19:06 +0900

    modify css load directory

--- gui/server.py
+++ gui/server.py
@@ -143,7 +143,7 @@ def index():
 
 @route("/css/<file_name>")
 def css(file_name):
-    return static_file("css/" + file_name, root='./')
+    return static_file("css/" + file_name, root=BASE_DIR + '/css')
 
 
 @route("/static/<file_name>")
