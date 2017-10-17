b3bbdda6d1eec2e81ea66dbd178025819a19fe44
suwa <suwa@gridsolar.jp>
2017-10-17 18:11:34 +0900

    WIP: Created CV module

--- renom/utility/cross_validate.py
+++ renom/utility/cross_validate.py
@@ -1,6 +1,7 @@
 import numpy as np
 import renom as rm
 
+
 class CrossValidator():
 
     def __init__(self):
@@ -8,7 +9,10 @@ class CrossValidator():
 
     def validate(self, trainer, train_distributor, test_distributor, k=4):
         data_size = len(train_distributor)
-        one_data_size = data_size//k
+        one_data_size = data_size // k
+        results = []
         for i in range(k):
-            train_dist, test_dist = None, None
-            trainer.train(train_distributor, test_distributor)
+            train_dist, test_dist = train_dist[i * one_data_size:(i + 1) * one_data_size]
+            trainer.train(train_dist, test_dist)
+            results.append(trainer.test(test_dist))
+        return np.mean(results)
