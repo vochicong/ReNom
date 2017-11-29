6bdfd46ea77c5951293814356be500cd6b23513d
suwa <suwa@gridsolar.jp>
2017-11-29 14:56:10 +0900

    Modified dir structure

--- doc/build.sh
+++ doc/build.sh
@@ -1,14 +1,14 @@
 # document will be created at _build/html
 
 make clean -e SPHINXOPTS="-D language='ja'" html
-python rewrite-tutorial.py ja
+python notebooks/rewrite_tutorial.py ja
 mv _build/html _build/ja
 
 make -e SPHINXOPTS="-D language='en'" html
-python rewrite-tutorial.py en
+python notebooks/rewrite_tutorial.py en
 mv _build/ja _build/html/ja
 
-python rewrite_path.py
+python notebooks/rewrite_path.py
 
 rm -r _build/html/old-versions
 rm -r _build/html/ja/old-versions
--- doc/tutorial_dir_title_conf.py
+++
@@ -1,9 +0,0 @@
-# dir_name, ja_title, en_title
-dir_title_config = [
-
-    ["basic", "基本機能", "Basic"],
-    ["preprocessing", "前処理", "Preprocessing"],
-    ["time_series", "時系列データ", "Time Series Data"],
-    ["image_processing", "画像データ", "Image Data"],
-    ["clustering", "クラスタリング", "Clustering"]
-]
