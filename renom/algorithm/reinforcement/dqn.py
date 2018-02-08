f061d80e279ee45a34101de231b5730fb419ef92
Grid dev team <dev_team@gridsolar.jp>
2018-02-08 01:30:28 +0000

    Modify doc

--- renom/algorithm/reinforcement/dqn.py
+++ renom/algorithm/reinforcement/dqn.py
@@ -16,18 +16,18 @@ class DQN(object):
     Args:
         q_network (Model): Q-Network.
         state_size (tuple, list): The size of state.
-        action_pattern (int): The number ob action pattern.
-        ganma (float): Discount rate.
+        action_pattern (int): The number of action pattern.
+        gamma (float): Discount rate.
         buffer_size (float, int): The size of replay buffer.
     """
 
-    def __init__(self, q_network, target_q, state_size, action_pattern, ganma=0.99, buffer_size=1e5):
+    def __init__(self, q_network, target_q, state_size, action_pattern, gamma=0.99, buffer_size=1e5):
         self._network = q_network
         self._target_network = target_q
         self._action_size = action_pattern
         self._state_size = state_size if hasattr(state_size, "__getitem__") else [state_size, ]
         self._buffer_size = buffer_size
-        self._ganma = ganma
+        self._gamma = gamma
         self._buffer = ReplayBuffer([1, ], self._state_size, buffer_size)
 
     def action(self, state):
@@ -107,7 +107,7 @@ class DQN(object):
             >>> dqn = DQN(model,
             ...           state_size=state_size,
             ...           action_pattern=action_pattern,
-            ...           ganma=0.99,
+            ...           gamma=0.99,
             ...           buffer_size=buffer_size)
             >>>
             >>> # Training
@@ -199,7 +199,7 @@ class DQN(object):
 
                     # train_state = train_state.reshape(batch_size, *self._state_size)
                     value = self._target_network(train_state).as_ndarray(
-                    ) * self._ganma * (~train_terminal[:, None])
+                    ) * self._gamma * (~train_terminal[:, None])
 
                     for i in range(batch_size):
                         a = train_action[i, 0].astype(np.integer)
