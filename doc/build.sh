196795cf6e41a4a71547e3eb7fbed18335483c0c
Grid dev team <dev_team@gridsolar.jp>
2018-02-08 08:14:21 +0000

    Modified doc build.sh

--- doc/build.sh
+++ doc/build.sh
@@ -1,5 +1,18 @@
 # document will be created at _build/html
 
+DIR_STATIC="_static/jsMath-3.6e"
+URL_MATH="https://jaist.dl.sourceforge.net/project/jsmath/jsMath/3.6e/jsMath-3.6e.zip"
+ZIP_NAME="jsMath-3.6e.zip"
+
+if [ -d ${DIR_STATIC} ]; then
+    echo "jsMath exists."
+else
+    echo "Downloading jsMath"
+    wget ${URL_MATH} 
+    unzip -d _static ${ZIP_NAME} 
+    rm ${ZIP_NAME}
+fi
+
 sphinx-versioning build -w doc_v2.4.1 -W nothing -r doc_v2.4.1 doc _build/html -- -D language='en'
 sphinx-versioning build -w doc_v2.4.1 -W nothing -r doc_v2.4.1 doc _build/html/ja -- -D language='ja'
 
