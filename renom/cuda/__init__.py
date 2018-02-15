6467fcd74ba86fc4f77b4f31176aa9e7dcaab616
suwa <suwa@gridsolar.jp>
2018-02-15 15:20:48 +0900

    Modified curand.

--- renom/cuda/__init__.py
+++ renom/cuda/__init__.py
@@ -9,7 +9,6 @@ try:
     from renom.cuda.curand import *
     _has_cuda = True
 except ImportError as e:
-    warnings.warn("Couldn't find cuda modules. %s" % e)
     curand_generator = None
     _has_cuda = False
 
@@ -92,11 +91,14 @@ _CuRandGens = {}
 
 def curand_generator(seed=None):
     deviceid = cuGetDevice()
-    if deviceid in _CuRandGens:
-        return _CuRandGens[deviceid]
-
     if seed is None:
-        seed = np.random.randint(0, int(1e5))
+        seed = seed if seed else np.random.randint(4294967295, size=1)
+
+    if deviceid in _CuRandGens:
+        gen = _CuRandGens[deviceid]
+        gen.set_seed(seed)
+        return gen
+
     ret = CuRandGen(seed)
     _CuRandGens[deviceid] = ret
     return ret
