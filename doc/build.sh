e81a63dbaead30db606298d3e77b1155bb9a0e43
Grid dev team <dev_team@gridsolar.jp>
2018-02-16 06:03:47 +0000

    Modified build settings.

--- doc/build.sh
+++ doc/build.sh
@@ -1,24 +1,26 @@
 # document will be created at _build/html
 
 DIR_STATIC="_static/jsMath-3.6e"
-URL_MATH="https://jaist.dl.sourceforge.net/project/jsmath/jsMath/3.6e/jsMath-3.6e.zip"
-ZIP_NAME="jsMath-3.6e.zip"
 
 if [ -d ${DIR_STATIC} ]; then
     echo "jsMath exists."
 else
     echo "Downloading jsMath"
-    wget ${URL_MATH} 
-    unzip -d _static ${ZIP_NAME} 
-    rm ${ZIP_NAME}
+    unzip -d _static ../../jsMath-3.6e.zip
 fi
 
-sphinx-versioning build -W 'v2.4.1' -w nothing -r v2.4.1 doc _build/html -- -D language='en'
+rm -rf _build/html
+
+sphinx-versioning build -W 'v2.4.1' -w 'aaa' -r 'v2.4.1' doc _build/html -- -D language='en'
 python rewrite_tutorial.py en
+
 mv _build/html _build/en
 
-sphinx-versioning build -W 'v2.4.1' -w nothing -r v2.4.1 doc _build/html -- -D language='ja'
+sphinx-versioning build -W 'v2.4.1' -w 'aaa' -r 'v2.4.1' doc _build/html -- -D language='ja'
 python rewrite_tutorial.py ja
+for f in `find _build -type d | grep 'static' | grep -v 'static/'`; do
+    cp -r _static/* ${f}
+done
 mv _build/html _build/ja
 
 mv _build/en _build/html
