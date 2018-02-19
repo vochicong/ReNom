707e795f44e1f906f27d985ee82bac3fac33c639
Grid dev team <dev_team@gridsolar.jp>
2018-02-19 01:36:09 +0000

    Add doc revising py files.

---
+++ doc/tutorial_dir_title_conf.py
@@ -0,0 +1,16 @@
+# dir_name, ja_title, en_title
+dir_title_config = [
+
+    ["neuralnetwork", "ニューラルネットワークの概要とリンク", "NeuralNetwork Abstract and Links for each page"],
+    ["basic", "基本機能", "Basic"],
+    ["preprocessing", "前処理", "Preprocessing"],
+    #["feature_extraction", "特徴量抽出", "Feature Extraction"],
+    ["time_series", "時系列データ", "Time Series Data"],
+    ["image_processing", "画像データ", "Image Data"],
+    ["clustering", "クラスタリング", "Clustering"],
+    ["regression", "回帰", "Regression"],
+    ["generative-model", "生成モデル", "Generative Model"],
+    ["tda", "TDA 基本機能", "TDA Basic"],
+    ["tda-case-study", "TDAケーススタディ", "TDA Case Study"]
+
+]
