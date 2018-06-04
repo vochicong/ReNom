5cd03d87bfe307ae959031ec4df7e9377fa924e0
Carl <kjaergaard.carl@gridsolar.jp>
2018-06-04 11:34:24 +0900

    Updated Curand set seed

--- renom/cuda/curand.pyx
+++ renom/cuda/curand.pyx
@@ -23,6 +23,7 @@ class CuRandGen(object):
 
     def __init__(self, seed=None, rng_type=CURAND_RNG_PSEUDO_DEFAULT):
         self.r_type = rng_type
+        self.set_generator()
         self.set_seed(seed)
 
     def rand_uniform(self, gpu_value):
@@ -38,11 +39,11 @@ class CuRandGen(object):
 
     def rand_normal(self, gpu_value, mean=0.0, std=1.0):
         cdef curandGenerator_t gen = <curandGenerator_t><uintptr_t>self.gen
-        cdef void* ptr = <void*><uintptr_t>gpu_value
+        cdef void* ptr = <void*><uintptr_t>gpu_value._ptr
         if precision == np.float32:
-            curandGenerateNormal(gen, <float*>ptr, <size_t>gpu_value.size, <float>mean, <float>std)
+            curand_check(curandGenerateNormal(gen, <float*>ptr, <size_t>gpu_value.size, <float>mean, <float>std))
         elif precision == np.float64:
-            curandGenerateNormalDouble(gen, <double*>ptr, <size_t>gpu_value.size, <double>mean, <double>std)
+            curand_check(curandGenerateNormalDouble(gen, <double*>ptr, <size_t>gpu_value.size, <double>mean, <double>std))
         else:
             raise Exception("{} is not supported precision".format(precision))
         return
@@ -52,14 +53,18 @@ class CuRandGen(object):
         cubinarize(gpu_value, prob, gpu_value)
         return
 
-    def set_seed(self, seed):
-        gen = getattr(self, "gen", None) 
+    def set_generator(self):
+        gen = getattr(self, "gen", None)
         if gen:
             destroyCurandGenerator(self.gen)
         gen = createCurandGenerator(self.r_type)
         cdef curandGenerator_t generator = <curandGenerator_t><uintptr_t>gen
-        curand_check(curandSetPseudoRandomGeneratorSeed(generator, <unsigned long long>seed))
-        self.gen = gen
+        self.gen = <uintptr_t> gen
+        return
+
+    def set_seed(self, seed):
+        curand_check(curandSetPseudoRandomGeneratorSeed(<curandGenerator_t><uintptr_t> self.gen, <unsigned long long>seed))
+        curand_check(curandSetGeneratorOffset(<curandGenerator_t><uintptr_t> self.gen, 0))
         return
 
     def __del__(self):
