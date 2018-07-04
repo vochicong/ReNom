f2a1bf687a832fb48d3e9db6a515e05ca36c6630
suwa <suwa@gridsolar.jp>
2018-07-04 17:01:55 +0900

    Modify module import

--- renom/utility/distributor/distributor.py
+++ renom/utility/distributor/distributor.py
@@ -6,7 +6,6 @@ import numpy as np
 from renom.core import get_gpu, Node
 from renom.cuda import is_cuda_active
 from renom.config import precision
-import renom.cuda.cuda_base as cuda_base
 
 
 class Distributor(object):
@@ -167,6 +166,7 @@ class GPUDistributor(Distributor):
 
     def __init__(self, x, y, **kwargs):
         assert is_cuda_active(), "Cuda must be activated to use GPU distributor"
+        import renom.cuda.cuda_base as cuda_base
         super(GPUDistributor, self).__init__(x=x, y=y, data_table=kwargs.get("data_table"))
         assert len(x) == len(y), "Input batches must have same number as output batches"
         self._data_size = len(x)
