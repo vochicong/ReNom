c0a8cba5e9f1361d483d2b423da0bb298966d34a
suwa <suwa@gridsolar.jp>
2018-10-19 15:16:19 +0900

    Removed comment

--- renom/layers/function/unpool2d.py
+++ renom/layers/function/unpool2d.py
@@ -26,35 +26,6 @@ class max_unpool2d(Node):
     def __new__(cls, x, prev_pool):
         return cls.calc_value(x, prev_pool._item)
 
-    #@classmethod
-    # def _oper_cpu(cls, x, prev_pool):
-    #    N = len(x)
-    #    col = np.zeros((N, prev_pool.attrs._in_shape[0], prev_pool.attrs._kernel[0],
-    #                    prev_pool.attrs._kernel[1], prev_pool.attrs._out_shape[1], prev_pool.attrs._out_shape[2]))
-    #    col_r1 = np.zeros((N, prev_pool.attrs._in_shape[0], prev_pool.attrs._kernel[0],
-    #                    prev_pool.attrs._kernel[1], prev_pool.attrs._out_shape[1], prev_pool.attrs._out_shape[2]))
-
-    #    col_k = np.rollaxis(col.reshape(
-    #        N, prev_pool.attrs._in_shape[0], -1, prev_pool.attrs._out_shape[1], prev_pool.attrs._out_shape[2]), 2)
-
-    #    for i in np.ndindex(N, prev_pool.attrs._in_shape[0],
-    #                        prev_pool.attrs._out_shape[1], prev_pool.attrs._out_shape[2]):
-    #        col_k[prev_pool.attrs._index[i]][i] = x[i]
-
-    #    ret = col2im(col, prev_pool.attrs._in_shape[1:],
-    #                 prev_pool.attrs._stride, prev_pool.attrs._padding)
-
-    #    revs = np.zeros_like(ret)
-    #    revs[ret != 0] = 1
-
-    #    ret = cls._create_node(ret)
-    #    ret.attrs._x = x
-    #    ret.attrs._stride = prev_pool.attrs._stride
-    #    ret.attrs._padding = prev_pool.attrs._padding
-    #    ret.attrs._filter = prev_pool.attrs._kernel
-    #    ret.attrs._revs = revs
-    #    return ret
-
     _oper_cpu = max_unpoolnd._oper_cpu
 
     _oper_gpu = max_unpoolnd._oper_gpu
@@ -68,20 +39,6 @@ class average_unpool2d(Node):
 
     def __new__(cls, x, prev_pool):
         return cls.calc_value(x, prev_pool._item)
-
-    #@classmethod
-    # def _oper_cpu(cls, x, prev_pool):
-    #    N = len(x)
-    #    col = np.zeros((N, prev_pool.attrs._in_shape[0], prev_pool.attrs._kernel[0],
-    #                    prev_pool.attrs._kernel[1], prev_pool.attrs._out_shape[1], prev_pool.attrs._out_shape[2]))
-    #    col_k = np.rollaxis(col.reshape(
-    #        N, prev_pool.attrs._in_shape[0], -1, prev_pool.attrs._out_shape[1], prev_pool.attrs._out_shape[2]), 2)
-    #    col_k[:] = x / float(len(col_k))
-    #    dx = col2im(col, prev_pool.attrs._in_shape[1:],
-    #                prev_pool.attrs._stride, prev_pool.attrs._padding)
-    #    ret = cls._create_node(dx)
-    #    ret.attrs._x = x
-    #    return ret
 
     _oper_cpu = average_unpoolnd._oper_cpu
 
@@ -113,22 +70,7 @@ class MaxUnPool2d:
     def __init__(self):
         pass
 
-    def __call__(self, x, prev_pool=None):
-        #p = x
-        # if prev_pool:
-        #    assert isinstance(prev_pool, max_pool2d)
-        # while prev_pool is None:
-        #    if isinstance(p, max_pool2d) and p.shape == x.shape:
-        #        prev_pool = p
-        #    else:
-        #        try:
-        #            p = p.attrs._x
-        #        except AttributeError:
-        #            raise Exception("Could not find previous 2D max pool")
-        # Currently, ReNom expects all Node arguments as input to other nodes
-        # to be called later with _update_diff at least once per use.
-        # We hide the 2d pool from ReNom by inserting it into the SimpleContainer
-        # class, where ReNom does not look for Node inputs.
+    def __call__(self, x, prev_pool):
         return self.forward(x, SimpleContainer(prev_pool))
 
     def forward(self, x, prev_pool):
